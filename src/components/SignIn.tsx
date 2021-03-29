ea1ef445929c2beeae76d63ea8a9bca227294d9f
Kurt <3249028+kurt1288@users.noreply.github.com>
2021-03-29 13:19:29 -0700

    fix width of login fields

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -64,13 +64,13 @@ const GetApiKey = () => {
                   <div className="border border-gray-700 rounded p-7 px-10 shadow-lg bg-gray-900">
                      <form className="mt-2">
                         <div>
-                           <input type="text" placeholder="Username" className="text-gray-700 py-2 px-2 box-border border border-gray-400 rounded" onChange={(e) => { setUserName(e.target.value); validate(); }} required />
+                           <input type="text" placeholder="Username" className="w-full text-gray-700 py-2 px-2 box-border border border-gray-400 rounded" onChange={(e) => { setUserName(e.target.value); validate(); }} required />
                         </div>
                         <div className="mt-3">
-                           <input type="text" placeholder="Token" className="text-gray-700 py-2 px-2 box-border border border-gray-400 rounded" onChange={(e) => { setUserToken(e.target.value); validate(); }} />
+                           <input type="text" placeholder="Token" className="w-full text-gray-700 py-2 px-2 box-border border border-gray-400 rounded" onChange={(e) => { setUserToken(e.target.value); validate(); }} />
                         </div>
                         <div className="mt-3">
-                           <button type="submit" className="py-2 px-4 w-full text-white bg-green-600 rounded hover:bg-green-700 disabled:opacity-50 disabled:cursor-default disabled:bg-green-600" disabled={disabled} onClick={Login}>Login</button>
+                           <button type="submit" className="py-2 px-2 w-full text-white bg-green-600 rounded hover:bg-green-700 disabled:opacity-50 disabled:cursor-default disabled:bg-green-600" disabled={disabled} onClick={Login}>Login</button>
                         </div>
                      </form>
                      <p className="text-sm mt-4 text-center">Need an account? <button type="button" className="text-yellow-500 hover:text-yellow-400" onClick={() => setSignIn(false)}>Register</button></p>
@@ -79,9 +79,9 @@ const GetApiKey = () => {
                : (
                   <div className="border border-gray-700 rounded p-7 px-10 shadow-lg bg-gray-900">
                      <form className="mt-2">
-                        <input type="text" placeholder="Username" className="text-gray-700 py-2 px-2 box-border border border-gray-400 rounded" onChange={(e) => { setUserName(e.target.value); validate(); }} required />
+                        <input type="text" placeholder="Username" className="w-full text-gray-700 py-2 px-2 box-border border border-gray-400 rounded" onChange={(e) => { setUserName(e.target.value); validate(); }} required />
                         <div className="mt-3">
-                           <button type="submit" className="py-2 px-4 w-full text-white bg-green-600 rounded hover:bg-green-700 disabled:opacity-50 disabled:cursor-default disabled:bg-green-600" disabled={disabled} onClick={getApiKey}>Sign Up</button>
+                           <button type="submit" className="py-2 px-2 w-full text-white bg-green-600 rounded hover:bg-green-700 disabled:opacity-50 disabled:cursor-default disabled:bg-green-600" disabled={disabled} onClick={getApiKey}>Sign Up</button>
                         </div>
                      </form>
                      <p className="text-sm mt-4 text-center">Already have an account? <button type="button" className="text-yellow-500 hover:text-yellow-400" onClick={() => setSignIn(true)}>Sign in</button></p>
