10b221043f9832326acfb53e9aad1933028b9e18
Kurt <3249028+kurt1288@users.noreply.github.com>
2021-04-06 21:13:26 -0700

    remove unnecessary commented code

--- src/components/Ships/ShipCard.tsx
+++ src/components/Ships/ShipCard.tsx
@@ -153,21 +153,6 @@ const ShipCard = ({
                      </div>
                   </div>
                </div>
-               {/* {automation?.enabled
-                  ? (
-                     <div className="flex items-center justify-end">
-                        {automation.error
-                        && (
-                           <div className="w-6 h-6 mr-1 pt-0.5" title="Automation error">
-                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#EF4444">
-                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.8} d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-                              </svg>
-                           </div>
-                        )}
-                        <p className="text-xs">Automating</p>
-                        <button type="button" className="text-xs ml-2 px-2 py-1 bg-purple-500 rounded hover:bg-purple-600" onClick={() => setAutomateModalShow(true)}>Tasks</button>
-                     </div>
-                  ) : null} */}
                <div className="py-3 px-3 pl-2 border-b border-gray-700 relative">
                   {(flightPlan && isFuture(new Date(flightPlan.arrivesAt))) || !ship.location
                      ? (
